c940229bbb1f2f0789446b75b074ddf91ccec7f4
DenDen047 <sh.mn.nat@gmail.com>
2019-03-16 11:53:07 +0900

    特定ファイルの画像パスを取り出すことができた

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
 import os
 import sys
+import re
+import glob
 
 
 def main():
-    print('hello main.py')
+    # 全ての画像をロード
+    files = glob.glob('/data/*')
+    images = []
+    for f in files:
+        if re.search('.+.(jpg|png)', f):
+            images += [f]
+    print(images)
+
+    # 画像を結合
+
+    # 保存
 
 if __name__ == '__main__':
     main()
